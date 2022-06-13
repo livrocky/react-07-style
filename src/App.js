d806c1d60bda62cb26991cd498041a99f89d39e8
rokas <rokasra@gmail.com>
2022-06-13 09:40:42 +0300

    asdasd

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import './App.css';
 import Header from './components/header/Header';
 
 function App() {
+  const isLarge = false;
+
+  const btnStyles = {
+    background: 'blue',
+    fontSize: isLarge ? '25px' : '10px',
+  };
   return (
     <div className='App'>
       <Header />
+      <h1>Hello again world</h1>
+      <buttons tyle={btnStyles}> Click me </buttons>
     </div>
   );
 }
